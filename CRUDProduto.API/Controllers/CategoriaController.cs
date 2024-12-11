94695b97d0cf7a6ae58610aff72e2214f2e2a649
Vitor Emanuel <vitorejobdias@gmail.com>
2024-12-11 01:24:25 -0300

    Criação das controllers de produto e categoria

---
+++ CRUDProduto.API/Controllers/CategoriaController.cs
@@ -0,0 +1,31 @@
+﻿using CRUDProduto.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using CRUDProduto.Core.Dtos;
+
+namespace CRUDProduto.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        private readonly ICategoriaAppService _categoriaAppService;
+        public CategoriaController(ICategoriaAppService categoriaAppService)
+        {
+            _categoriaAppService = categoriaAppService;
+        }
+
+        [Route("GetAllCategoriasAsync")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoriaDTO>>> GetAllCategoriasAsync()
+        {
+            try
+            {
+                return Ok(await _categoriaAppService.GetCategoriasAsync());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Ocorreu um erro ao buscar todas as categorias!");
+            }
+        }
+    }
+}
---
+++ CRUDProduto.API/Controllers/ProdutoController.cs
@@ -0,0 +1,31 @@
+﻿using CRUDProduto.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using CRUDProduto.Core.Dtos;
+
+namespace CRUDProduto.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly IProdutoAppService _produtoAppService;
+        public ProdutoController(IProdutoAppService produtoAppService)
+        {
+            _produtoAppService = produtoAppService;
+        }
+
+        [Route("GetAllProdutosAsync")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProdutoDTO>>> GetAllProdutosAsync()
+        {
+            try
+            {
+                return Ok(await _produtoAppService.GetProdutosAsync());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Ocorreu um erro ao buscar todos os produtos!");
+            }
+        }
+    }
+}
