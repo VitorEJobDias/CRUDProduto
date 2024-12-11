5846ef05c56d6a5480fe35948d32f67ed2bd8a0f
Vitor Emanuel <vitorejobdias@gmail.com>
2024-12-11 02:16:09 -0300

    Migration de criação das tabelas feita

---
+++ CRUDProduto.Infrastructure/Persistence/Migrations/20241211051355_InitialCreate.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUDProduto.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tbCategoria",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbCategoria", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tbProduto",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Preco = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    IdCategoria = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbProduto", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_tbProduto_tbCategoria_Id",
+                        column: x => x.Id,
+                        principalTable: "tbCategoria",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tbProduto");
+
+            migrationBuilder.DropTable(
+                name: "tbCategoria");
+        }
+    }
+}
