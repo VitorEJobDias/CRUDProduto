2383694dd14609d3d473ff75dd2e38084db047b4
Vitor Emanuel <vitorejobdias@gmail.com>
2024-12-11 00:16:53 -0300

    Criação do ProdutoService e sua lógica de negócio

---
+++ CRUDProduto.Core/Services/IProdutoService.cs
@@ -0,0 +1,13 @@
+﻿using CRUDProduto.Core.Entities;
+
+namespace CRUDProduto.Core.Services
+{
+    public interface IProdutoService
+    {
+        Task<IEnumerable<Produto?>> GetProdutosAsync();
+        Task<Produto?> GetProdutoByIdAsync(Guid id);
+        Task CreateProdutoAsync(Produto produto);
+        Task DeleteProdutoAsync(Guid id);
+        Task UpdateProdutoAsync(Produto produto);
+    }
+}
---
+++ CRUDProduto.Infrastructure/Persistence/Services/ProdutoService.cs
@@ -0,0 +1,40 @@
+﻿using CRUDProduto.Core.Repositories;
+using CRUDProduto.Core.Services;
+using CRUDProduto.Core.Entities;
+
+namespace CRUDProduto.Infrastructure.Persistence.Services
+{
+    internal class ProdutoService : IProdutoService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProdutoService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<Produto?>> GetProdutosAsync() =>
+            await _unitOfWork.Produto.GetProdutosAsync();
+
+        public async Task<Produto?> GetProdutoByIdAsync(Guid id) =>
+            await _unitOfWork.Produto.GetProdutoByIdAsync(id);
+
+        public async Task CreateProdutoAsync(Produto produto)
+        {
+            await _unitOfWork.Produto.CreateProdutoAsync(produto);
+            await _unitOfWork.SaveChangesAsync();
+        }
+
+        public async Task DeleteProdutoAsync(Guid id)
+        {
+            await _unitOfWork.Produto.DeleteAsync(id);
+            await _unitOfWork.SaveChangesAsync();
+        }
+
+        public async Task UpdateProdutoAsync(Produto produto)
+        {
+            _unitOfWork.Produto.UpdateProduto(produto);
+            await _unitOfWork.SaveChangesAsync();
+        }
+    }
+}
