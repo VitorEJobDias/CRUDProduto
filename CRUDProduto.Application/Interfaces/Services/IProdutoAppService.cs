2c1ab89c701e1356eb648ea74af866e47f6bbb3b
Vitor Emanuel <vitorejobdias@gmail.com>
2024-12-11 01:17:46 -0300

    Separação de responsabilidade por parte de Produto; Criada classe e interface na camada Application;

---
+++ CRUDProduto.Application/Interfaces/Services/IProdutoAppService.cs
@@ -0,0 +1,13 @@
+﻿using CRUDProduto.Core.Dtos;
+
+namespace CRUDProduto.Application.Interfaces.Services
+{
+    public interface IProdutoAppService
+    {
+        Task CreateProdutoAsync(ProdutoDTO produto);
+        Task DeleteProdutoAsync(Guid id);
+        Task UpdateProdutoAsync(ProdutoDTO produto);
+        Task<IEnumerable<ProdutoDTO>> GetProdutosAsync();
+        Task<ProdutoDTO> GetProdutoByIdAsync(Guid id);
+    }
+}
---
+++ CRUDProduto.Application/Services/ProdutoAppService.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using CRUDProduto.Core.Dtos;
+using CRUDProduto.Core.Entities;
+using CRUDProduto.Core.Services;
+using CRUDProduto.Application.Interfaces.Services;
+
+namespace CRUDProduto.Application.Services
+{
+    public class ProdutoAppService : IProdutoAppService
+    {
+        private readonly IProdutoService _produtoService;
+        private readonly IMapper _mapper;
+
+        public ProdutoAppService(IProdutoService produtoService, IMapper mapper)
+        {
+            _produtoService = produtoService;
+            _mapper = mapper;
+        }
+
+        public async Task CreateProdutoAsync(ProdutoDTO produto)
+        {
+            var produtoEntity = _mapper.Map<Produto>(produto);
+            await _produtoService.CreateProdutoAsync(produtoEntity);
+        }
+
+        public async Task DeleteProdutoAsync(Guid id)
+        {
+            await _produtoService.DeleteProdutoAsync(id);
+        }
+
+        public async Task UpdateProdutoAsync(ProdutoDTO produto)
+        {
+            var produtoEntity = _mapper.Map<Produto>(produto);
+            await _produtoService.UpdateProdutoAsync(produtoEntity);
+        }
+
+        public async Task<IEnumerable<ProdutoDTO>> GetProdutosAsync()
+        {
+            var produtos = await _produtoService.GetProdutosAsync();
+            return _mapper.Map<IEnumerable<ProdutoDTO>>(produtos);
+        }
+
+        public async Task<ProdutoDTO> GetProdutoByIdAsync(Guid id)
+        {
+            var produto = await _produtoService.GetProdutoByIdAsync(id);
+            return _mapper.Map<ProdutoDTO>(produto);
+        }
+    }
+}
