b12cc072757d9e24c3d842beebd4f8ca1afe173a
Vitor Emanuel <vitorejobdias@gmail.com>
2024-12-11 00:22:15 -0300

    Criação das DTOs

---
+++ CRUDProduto.Core/Dtos/CategoriaDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUDProduto.Core.Dtos
+{
+    public class CategoriaDTO
+    {
+        public Guid Id { get; set; }
+        public bool Active { get; set; }
+        public string? Nome { get; set; }
+        public ICollection<ProdutoDTO>? Produtos { get; set; }
+    }
+}
---
+++ CRUDProduto.Core/Dtos/ProdutoDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace CRUDProduto.Core.Dtos
+{
+    public class ProdutoDTO
+    {
+        public Guid Id { get; set; }
+        public bool Active { get; set; }
+        public string? Nome { get; private set; }
+        public decimal Preco { get; private set; }
+        public Guid IdCategoria { get; private set; }
+    }
+}
