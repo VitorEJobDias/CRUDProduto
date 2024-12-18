db277de5970c0e6f82a46e9062b5c81c8b545155
Vitor Emanuel <vitorejobdias@gmail.com>
2024-12-17 21:33:40 -0300

    Inicio da implementação de Testes Unitários utilizando xUnit

---
+++ CRUDProduto.Tests/MockData/MockObjects.cs
@@ -0,0 +1,45 @@
+﻿using CRUDProduto.Application.Interfaces.Services;
+using CRUDProduto.Core.Repositories;
+using CRUDProduto.Core.Services;
+using Moq;
+
+namespace CRUDProduto.Tests.MockData
+{
+    public static class MockObjects
+    {
+        public static Mock<IProdutoRepository> GetMockProdutoRepository()
+        {
+            return new Mock<IProdutoRepository>();
+        }
+
+        public static Mock<ICategoriaRepository> GetMockCategoriaRepository()
+        {
+            return new Mock<ICategoriaRepository>();
+        }
+
+        public static Mock<IUnitOfWork> GetMockUnitOfWork()
+        {
+            return new Mock<IUnitOfWork>();
+        }
+
+        public static Mock<IProdutoService> GetMockProdutoService()
+        {
+            return new Mock<IProdutoService>();
+        }
+
+        public static Mock<ICategoriaService> GetMockCategoriaService()
+        {
+            return new Mock<ICategoriaService>();
+        }
+
+        public static Mock<IProdutoAppService> GetMockProdutoAppService()
+        {
+            return new Mock<IProdutoAppService>();
+        }
+
+        public static Mock<ICategoriaAppService> GetMockCategoriaAppService()
+        {
+            return new Mock<ICategoriaAppService>();
+        }
+    }
+}
---
+++ CRUDProduto.Tests/UnitTests/CategoriaUnitTests/GetCategoriaTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDProduto.Tests.UnitTests.CategoriaUnitTests
+{
+    public class GetCategoriaTests
+    {
+    }
+}
